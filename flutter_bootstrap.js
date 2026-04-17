71a2d15e9bc2e69cf2569d171c39d5ab63d365ec
Charbel Khoury <charbelkhoury99@gmail.com>
2026-04-17 03:54:56 +0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "529716667"
+    serviceWorkerVersion: "1398100211"
   }
 });
