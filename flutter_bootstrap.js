89273215c1fa3b6436fcf3820c2d5ec8a5503eab
Charbel Khoury <charbelkhoury99@gmail.com>
2026-07-16 10:53:27 +0300

    fix for grid view pc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "374477473" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3786250236" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
